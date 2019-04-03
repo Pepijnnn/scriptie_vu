50b32ddcfcf8cddaebd2c74167440c4ad26129aa
pepijn <pepijn@sibbes.nl>
2019-04-03 11:09:58 +0200

    visualisation for bac over time and bac on a date

--- train_data/main.py
+++ train_data/main.py
@@ -1,18 +1,17 @@
 import pandas as pd
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
 import argparse
 
 from stack_for_visual import Store
 
-
 def main(**kwargs):
     lab_res = pd.read_csv('7 columns from mmi_Lab_MMI_Resistentie.txt', sep='\t', encoding="UTF-16")
-    bep_tekst = pd.read_csv('8 columns from mmi_Lab_MMI_BepalingenTekst.txt', sep='\t', encoding="UTF-16")
+    #bep_tekst = pd.read_csv('8 columns from mmi_Lab_MMI_BepalingenTekst.txt', sep='\t', encoding="UTF-16")
     isolaten = pd.read_csv('9 columns from mmi_Lab_MMI_Isolaten.txt', sep='\t', encoding="UTF-16")
-    opname = pd.read_csv('alle columns mmi_Opname_Opname.txt', sep='\t', encoding="UTF-16")
+    #opname = pd.read_csv('alle columns mmi_Opname_Opname.txt', sep='\t', encoding="UTF-16")
 
     s = Store()
-
+    
     mns = []
     for c, num in enumerate(lab_res["RISV_Waarde"]):
         if str(num) == "R":
@@ -26,14 +25,20 @@ def main(**kwargs):
 
     print(orgs)
     for date in mns:
-        print("On ", date[0], " we have found the following organisms: ", orgs)
-    # print(bep_tekst)
-    # print(isolaten)
-    # print(lab_res)
-    # print(opname)
+        for org in orgs:
+            print("On ", date[0], " we have found the following organisms: ", org)
+            s.add_bacteria_date(org,date[0])
+    
+    if kwargs["f"] == "date":
+        s.show_bacteria(kwargs["date"])
+    elif kwargs["f"] == "bac":
+        s.show_dates(kwargs["bac"])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Visualizer for antibiotic resistance in medical cultures')
+    parser.add_argument("--f", default="bac", help="select which function to use")
+    parser.add_argument("--date", default="25-8-2016", help="visualiser of AB resistance on a date with default date 25-8-2016, must be in format DD-MM-YYYY")
+    parser.add_argument("--bac", default="Escherichia coli", help="visualiser of AB resistance over time of a certain bacteria, default on 'Escherichia coli'")
     args = parser.parse_args()
 
 main(**vars(args))
--- train_data/stack_for_visual.py
+++ train_data/stack_for_visual.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplotlib
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+from matplotlib import dates as mdates
+import datetime as dt
+from collections import OrderedDict
 import cbor
 
 class Store:
@@ -13,6 +20,10 @@ class Store:
 		self.bac_list.append(bacteria)
 		self.dat_list.append(date)
 
+	def plot_bar_single_date(self, bac_count_dict):
+		plt.bar(range(len(bac_count_dict)), list(bac_count_dict.values()), tick_label=list(bac_count_dict.keys()), align='center')
+		plt.show()
+
 	# show the development of a specific bacteria between 2 dates
 	def show_bacteria_date(self, bacteria, date1, date2):
 		relevant_bac_date = []
@@ -23,17 +34,50 @@ class Store:
 		return cbor.loads(relevant_bac_date)
 
 	# show all the bacterias of a certain date
-	def show_bacteria(self, date1, date2):
-		relevant_bacs = []
-		for (bac, date) in self.bacteria_date:
-			 if date > date1 and date < date2:
-				 relevant_bacs.append((bac,date))
-		return cbor.loads(relevant_bacs)
+	def show_bacteria(self, date):
+		rel_bacs_y = []
+
+		for (bac, date_store) in self.bacteria_date:
+			if date_store == date:
+				for indiv in bac:
+					rel_bacs_y.append(indiv)
+		print(rel_bacs_y)
+		count_d = {x:rel_bacs_y.count(x) for x in rel_bacs_y}
+		self.plot_bar_single_date(count_d)
+
+	# IMPROVEMENT NEEDED FOR DATETIME
+	def plot_bar_single_bacteria(self, date_count_dict):
+		plt.bar(range(len(date_count_dict)), list(date_count_dict.values()), tick_label=list(date_count_dict.keys()), align='center')
+		plt.show()
 
 	# show the bacteria through the time
 	def show_dates(self, bacteria):
-		relevant_dates = []
-		for (bac, date) in self.bacteria_date:
-			if str(bac) == str(bacteria):
-				relevant_dates.append(date)
-		return cbor.loads(relevant_dates)
+		rel_dates = []
+		for (bacs, date) in self.bacteria_date:
+			for indiv_bac in bacs:
+				if str(indiv_bac) == str(bacteria):
+					rel_dates.append(date)
+
+		rel_dates.append("26-8-2016")
+		rel_dates.append("26-8-2018")
+		rel_dates.append("28-8-2016")
+		rel_dates.append("26-08-2016")
+		univ_dates = sorted([dt.datetime.strptime(d, '%d-%m-%Y').date() for d in rel_dates])
+
+		odict = OrderedDict()
+		for item in univ_dates:
+			try:
+				odict[item] += 1
+			except KeyError:
+				odict[item] = 1
+
+		self.plot_bar_single_bacteria(odict)
+		
+
+
+######### OLD CODE ############
+# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
+# plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+# plt.plot(univ_dates)
+# plt.gcf().autofmt.xdate()
+# exit()
