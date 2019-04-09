2f925b3000e84fde5d34669d7f5e404710c39b82
pepijn <pepijn@sibbes.nl>
2019-04-09 12:49:11 +0200

    Cluster met kleurtjes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 import argparse
+from sklearn import preprocessing
 import umap
 
 sel1_datatype = [["Monsternummer","StudieNummer","MateriaalShortName","WerkplekCode","BepalingCode","ArtsCode","AfdelingCodeAanvrager","Locatie","Waarde","Uitslag"],
@@ -38,28 +39,50 @@ def main(**kwargs):
             df1[col]= df1[col].fillna(df1[col].value_counts().idxmax())
         # df1['PhylumDivisionGroup'] = df1['PhylumDivisionGroup'].fillna(df1['PhylumDivisionGroup'].value_counts().idxmax())
         #print(df1['PhylumDivisionGroup'].value_counts().idxmax())
+        #print(df1)
+        df1 = df1.drop(['AfnameDatum'], axis=1)
+        df1 = df1.drop(['Pseudo_id'], axis=1)
         print(df1)
-        # one_hot_table = pd.get_dummies(df1)
-        # standard_embedding = umap.UMAP(random_state=42).fit_transform(one_hot_table)
-        # plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], s=30, cmap='Spectral')
-        # plt.show()
+        le = preprocessing.LabelEncoder()
+        labels = le.fit_transform(df1["ResAB_amount"])
+        one_hot_table = pd.get_dummies(df1)
+        standard_embedding = umap.UMAP(random_state=42).fit_transform(one_hot_table)
+        plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=labels, s=30, cmap='Spectral')
+        plt.show()
 
     def unsup_one_table(one_table):
         table = pd.DataFrame(one_table)
+        print(table.columns)
+        # for c, col in enumerate(table.columns):
+        #     if c > 2:
+        #         break
+        #     table = table.drop([col], axis=1)
+
         for col in table.columns:
             table[str(col)].fillna(method ='ffill', inplace = True) 
-
+        # if table['AfdelingNaamAanvrager'].loc(0):
+        #     print("JAAAAA")
+        #     table = table.drop(['AfdelingNaamAanvrager'], axis=1)
+        le = preprocessing.LabelEncoder()
+        labels = le.fit_transform(table["MicroOrganismeName"])
         one_hot_table = pd.get_dummies(table)
         standard_embedding = umap.UMAP(random_state=42).fit_transform(one_hot_table)
-        plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], s=30, cmap='Spectral')
+        plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=labels, s=30, cmap='Spectral')
         plt.show()
 
+    if kwargs["f"] == "a":
+        print("hoi")
+        unsup_one_table(tab_three)
+    elif kwargs["f"] == "b":
+        add_res_amount(tab_three, tab_one)  
     #unsup_one_table(tab_three)  
-    add_res_amount(tab_three, tab_one)  
+    # add_res_amount(tab_three, tab_one)  
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Unsupervised learning function')
-    parser.add_argument("--f", default="bac", help="select which function to use")
+    parser.add_argument("--f", default="a", help="select which function to use")
+    # parser.add_argument("--one", default="y", help="unsupervised learning over 1 table")
+    # parser.add_argument("--resadd", default = "n",  help="unsupervised learning over 1 table plus amount of res bacteria added per monstersample")
     args = parser.parse_args()
 
 main(**vars(args))
